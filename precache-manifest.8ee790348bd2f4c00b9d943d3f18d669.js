e241d13433d9be5714d863784d1a70583914ea8d
Timothy Asigbey <asigbeytimothy@gmail.com>
2020-07-31 02:16:44 +0800

    Updates

---
+++ precache-manifest.8ee790348bd2f4c00b9d943d3f18d669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7c9d2dda0603e6b41011fe88ecbb3a0",
+    "url": "/back-gen/index.html"
+  },
+  {
+    "revision": "4e8470e16f472d904724",
+    "url": "/back-gen/static/css/main.637f28a7.chunk.css"
+  },
+  {
+    "revision": "addab9ad8c8c40c96cb7",
+    "url": "/back-gen/static/js/2.58cce731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/back-gen/static/js/2.58cce731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8470e16f472d904724",
+    "url": "/back-gen/static/js/main.1756f0ea.chunk.js"
+  },
+  {
+    "revision": "afbdf47ee429294b5c21",
+    "url": "/back-gen/static/js/runtime-main.6dec0fa4.js"
+  }
+]);
